63982f2c9bda180514c41fd74326e00d67348677
huy88118 <huy88118@gmail.com>
2022-12-14 18:41:09 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+//Bài tập 1
+let A = 100.000; //Lương 1 ngày
+let B = 26;// Ngày công
+let C = A * B;
+console.log("C(lương nhân viên) = A(lương 1 ngày) * B(số ngày đi làm) =", C)
+console.log("Bài tập 1: Lương của nhân viên =",C);
+
+//Bài tập 2
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let f =  (a + b + c + d + e) / 5;
+console.log("Bài tập 2 : Giá trị trung bình =",f);
+console.log("Giá trị trung bình 5 số =",f);
+
+
+//Bài tập 3
+g = 23.500;
+i = 5;
+k = g * i;
+console.log("Bài tập 3 : Số tiền sau khi quy đổi =",k);
+console.log("k(số tiền sau quy đổi) = g(giá trị 1 usd) * i(số usd quy đổi) =",k);
+
+//Bài tập 4
+let dai = 15;
+let rong = 10;
+let dientich = dai * rong;
+console.log("Bài tập 4 :");
+console.log("Diện tích HCN =",dientich);//150
+let chuvi = (dai + rong) * 2
+console.log(" Chu vi HCN =",chuvi);//50
+
+//Bài tập 5
+console.log("Bài tập 5 :");
+let n = 83;
+let m = 83 % 10;
+console.log("m=",m); // 3
+n = Math.floor(n/10)
+console.log("n=",n);// 8
+let o = m + n;
+console.log("o = n + m :",o);//11
+
+
+let s = 26;
+let x = 26 % 10;
+console.log("x =",x);//6
+s = Math.floor(s/10)
+console.log("s =",s);//2
+w = s + x;
+console.log("w = s + x :",w);//8
+total = o + w;
+console.log("Tồng 2 kí số = o + w =",total);//19
